557d200fa20027c422529026965fec8488106bda
eduganu <eduard.ganau@gmail.com>
2019-12-19 00:08:19 +0100

    funciona el grafic de barres amb timestamps i lapsos de 5 minuts

--- src/app/tabsPages/tab4/tab4.page.ts
+++ src/app/tabsPages/tab4/tab4.page.ts
@@ -15,33 +15,47 @@ export class Tab4Page implements OnInit {
 registrosPulsaciones:BpmInfo[] = [];
 hoy:Date = environment.systemDate;
 
-@ViewChild("lineCanvas", {static:true}) barCanvas: ElementRef;
+dataDies = [{
+  x: new Date("2019/11/11"),
+  y: 1
+}, {
+  x: new Date("2019/11/12"),
+  y: 10
+}, {
+  x: new Date("2019/11/13"),
+  y: 5
+}]
+
+dataOriginal = {
+  //labels: ['S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8'],
+  datasets: [{
+    label: 'Pulsaciones por minuto',
+    data: this.dataDies,//[2.5, 3.8, 5, 6.9, 6.9, 7.5, 10, 17],
+    backgroundColor: 'rgb(38, 194, 129)', // array should have same number of elements as number of dataset
+    borderColor: 'rgb(38, 194, 129)',// array should have same number of elements as number of dataset
+    borderWidth: 1
+  }]
+}
+
+
+
+@ViewChild("barCanvas", {static:true}) barCanvas: ElementRef;
 
 private barChart: Chart;
 datos:any = {
   type: "bar",
-  data: {
-    labels: [], 
-    datasets: [
-      {
-        label: "pasos",
-        data: [],
-        
-        borderWidth: 1
-      }
-    ]
-  },
+  data: this.dataOriginal,
   options: {
     scales: {
-      yAxes: [
-        {
-          ticks: {
-            beginAtZero: true
-          }
-        }
-      ]
+        xAxes: [{
+            type: 'time',
+            distribution : 'series',
+            time: {
+                unit: 'hour'
+            }
+        }]
     }
-  }
+}
 }
   
 
@@ -51,11 +65,19 @@ datos:any = {
     this.userService.getPulsaciones(new Date(this.hoy.getTime() - 86400000),this.hoy).subscribe(datos => {
       this.registrosPulsaciones = datos;
       console.log(this.registrosPulsaciones)
-      this.datos.data = datos;
+      let parsedDatos = [];
+      datos.forEach(element => {
+        parsedDatos.push({x:element.datetime , y:element.bpm})
+        
+      });
+      this.dataOriginal.datasets[0].data = parsedDatos; // [{x:new Date(), y:1}]
+      //this.datos.data.datasets.data = datos;
       console.log(this.datos)
       
       this.initGraph(this.datos);
     })
+
+    //this.initGraph(this.datos);
   }
 
 
