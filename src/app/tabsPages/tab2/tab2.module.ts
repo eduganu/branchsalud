bf17a683f24d1bafd38af8392865e1cb98d36f85
Fihoca07 <Fihoca07@SINENSIA.EDU>
2019-12-18 08:08:51 +0100

    canvis al html del profile

--- src/app/tabsPages/tab2/tab2.module.ts
+++ src/app/tabsPages/tab2/tab2.module.ts
@@ -1,9 +1,11 @@
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
-import { NgModule } from '@angular/core';
+import { NgModule, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/models/User';
 
 @NgModule({
   imports: [
@@ -14,4 +16,25 @@ import { Tab2Page } from './tab2.page';
   ],
   declarations: [Tab2Page]
 })
-export class Tab2PageModule {}
+export class Tab2PageModule implements OnInit{
+  
+  user:User = new User();
+  
+  constructor(private userService: UserService) {
+    this.userService.getUserInfo().subscribe(datos => {
+      this.user = datos[0];
+    });
+  }
+  
+  ngOnInit(): void {
+    throw new Error("Method not implemented.");
+  }
+  
+
+  
+
+  
+
+
+
+}
